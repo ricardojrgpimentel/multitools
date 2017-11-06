381046623adcdf05b40aa59d8a1764797143c319
Ricardo Pimentel <ricardojrgpimentel@gmail.com>
2017-11-06 18:32:03 +0000

    added date converter layout

--- src/components/TimeBody/index.js
+++ src/components/TimeBody/index.js
@@ -1,7 +1,29 @@
 import React from 'react'
 import RealTime from './RealTime'
+import moment from 'moment'
 
 class TimeBody extends React.Component{
+
+  constructor(){
+    super()
+    this.state = {
+      date: moment().unix()
+    }
+    this.saveDate = this.saveDate.bind(this)
+    this.handleConvertDate = this.handleConvertDate.bind(this)
+  }
+
+  saveDate(e){
+    this.setState({
+      ...this.state,
+      date: e.target.value
+    })
+  }
+
+  handleConvertDate(){
+    console.log(this.state.date)
+    console.log(moment(this.state.date))
+  }
 
   render(){
     return(
@@ -9,6 +31,12 @@ class TimeBody extends React.Component{
         <div className="col-md-6">
           <div className="box">
             <RealTime />
+            <div className="input-wrapper" style={{justifyContent: 'space-around'}}>
+              <div>
+                <input onChange={(e) => {this.saveDate(e)}} type="text" value={this.state.date}/>
+              </div>
+              <button onClick={this.handleConvertDate}>Timestamp to Human Date</button>
+            </div>
           </div>
         </div>
       </div>
