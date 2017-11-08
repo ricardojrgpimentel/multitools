323c5b74281897f28b12e70c3fb503532c1cfc54
Ricardo Pimentel <ricardojrgpimentel@gmail.com>
2017-11-08 17:32:20 +0000

    added new path to main page

--- src/components/Main/Body/index.js
+++ src/components/Main/Body/index.js
@@ -9,22 +9,28 @@ class Body extends React.Component{
           <div className="box">
             <h2 className='main-title'>Choose one of the following tools</h2>
             <div className="row row-no-padding row-col-no-padding">
-              <div className="col-md-4 category-wrapper">
+              <div className="col-md-3 category-wrapper">
                 <Link to={'/cryptoconverter'}>
                   <img className='icon-category' src="/icons/ecommerce_euro.svg" alt=""/>
                   <h3 className='title'>Crypto Coins Converter</h3>
                 </Link>
               </div>
-              <div className="col-md-4 category-wrapper">
+              <div className="col-md-3 category-wrapper">
                 <Link to={'/timeconverter'}>
                   <img className='icon-category' src="/icons/basic_clock.svg" alt=""/>
                   <h3 className='title'>Timestamp Converter</h3>
                 </Link>
               </div>
-              <div className="col-md-4 category-wrapper">
+              <div className="col-md-3 category-wrapper">
                 <Link to={'/ratiocalculator'}>
                   <img className='icon-category' src="/icons/basic_display.svg" alt=""/>
                   <h3 className='title'>Screen Ratio Calculator</h3>
+                </Link>
+              </div>
+              <div className="col-md-3 category-wrapper">
+                <Link to={'/colortools'}>
+                  <img className='icon-category' src="/icons/software_eyedropper.svg" alt=""/>
+                  <h3 className='title'>Color Tools</h3>
                 </Link>
               </div>
             </div>
